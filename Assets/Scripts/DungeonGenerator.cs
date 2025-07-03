b7dc685f28634957444ea538bb7c0aea6c454148
SerBakon <serbakonyt@gmail.com>
2025-07-02 23:05:06 -0700

    Chore: finish character controller

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,221 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonGenerator : MonoBehaviour
+{
+    [SerializeField] private Vector3Int startPos = Vector3Int.zero;
+    [SerializeField] private int walkLength = 10;
+    [SerializeField] private int numRooms = 10;
+    [SerializeField] private int roomSize = 10;
+    [SerializeField] private bool roof = true;
+
+    [SerializeField] private GameObject floorTile;
+    [SerializeField] private GameObject starterTile;
+    [SerializeField] private GameObject wallTile;
+    [SerializeField] private GameObject doorTile;
+    [SerializeField] private GameObject doorX;
+    [SerializeField] private GameObject doorZ;
+    [SerializeField] private GameObject wallX;
+    [SerializeField] private GameObject wallZ;
+
+    private HashSet<Vector3Int> visited = new HashSet<Vector3Int>();
+    private HashSet<GameObject> tilesTotal = new HashSet<GameObject>();
+
+    private HashSet<Vector3Int> wallsTotal = new HashSet<Vector3Int>();
+    private HashSet<Vector3Int> doorsTotal = new HashSet<Vector3Int>();
+    private HashSet<Vector3Int> floorsTotal = new HashSet<Vector3Int>();
+
+    public static List<Vector3Int> cardinalDirectionsList = new List<Vector3Int>()
+    {
+        new Vector3Int(0,0,1), //UP
+        new Vector3Int(0,0,-1), //DOWN
+        new Vector3Int(1,0,0), //RIGHT
+        new Vector3Int(-1,0,0) //LEFT
+    };
+
+    void Start()
+    {
+        generate();
+    }
+
+    public void generate() {
+        clear();
+        //wallsTotal.Add(new Vector3Int(4,0,0));
+        starterGen(starterTile);
+        roomGen(numRooms, Vector3Int.zero);
+        //generate wall
+        foreach (var wall in wallsTotal) {
+            if (!(doorsTotal.Contains(wall))) {
+                GameObject spawnedTile = Instantiate(wallTile, new Vector3Int(wall.x, wall.y, wall.z) + Vector3Int.up, Quaternion.identity);
+                tilesTotal.Add(spawnedTile);
+                //wallObjectTotal.Add(spawnedTile); // Track the instantiated tiles
+            }
+            
+        }
+        //generate walls for starter
+        for (int i = -3; i <= 3; i++) {
+            if (i == 0) continue;
+            Instantiate(wallTile, new Vector3Int(i, 1, 3), Quaternion.identity);
+            Instantiate(wallTile, new Vector3Int(i, 1, -3), Quaternion.identity);
+            Instantiate(wallTile, new Vector3Int(-3, 1, i), Quaternion.identity);
+            Instantiate(wallTile, new Vector3Int(3, 1, i), Quaternion.identity);
+            wallsTotal.Add(new Vector3Int(i, 1, 3));
+            wallsTotal.Add(new Vector3Int(i, 1, -3));
+            wallsTotal.Add(new Vector3Int(-3, 1, i));
+            wallsTotal.Add(new Vector3Int(3, 1, i));
+        }
+        Instantiate(wallTile, new Vector3Int(-3, 1, 0), Quaternion.identity);
+        wallsTotal.Add(new Vector3Int(3, 1, 0));
+        fillDoors();
+    }
+
+    private void clear() {
+        foreach (var tile in tilesTotal) {
+            Destroy(tile); // Destroy old tiles from the scene
+        }
+        tilesTotal.Clear(); // Clear the list
+        visited.Clear();
+        wallsTotal.Clear();
+        doorsTotal.Clear();
+        floorsTotal.Clear();
+    }
+
+    private void tileGen(GameObject tile, HashSet<Vector3Int> floorPos) {
+        foreach (var floor in floorPos) {
+            GameObject spawnedTile = Instantiate(tile, new Vector3(floor.x, floor.y, floor.z), Quaternion.identity);
+            tilesTotal.Add(spawnedTile); // Track the instantiated tiles
+        }
+    }
+    private void roofGen(GameObject tile, HashSet<Vector3Int> floorPos) {
+        foreach (var floor in floorPos) {
+            GameObject spawnedTile = Instantiate(tile, new Vector3(floor.x, floor.y, floor.z) + Vector3Int.up * 2, Quaternion.identity);
+            tilesTotal.Add(spawnedTile); // Track the instantiated tiles
+        }
+    }
+    private void roomGen(int numRooms, Vector3Int center) {
+        HashSet<Vector3Int> floorPos = new HashSet<Vector3Int>();
+        for (int i = 0; i < numRooms; i++) {
+            // doesn't change center for the 3 starter rooms.
+            if (center == Vector3Int.zero) {
+                center = wallsTotal.ElementAt(Random.Range(0, wallsTotal.Count));
+            }
+            floorPos = RandomWalk.randomWalk(center, walkLength, roomSize, visited);
+
+            if (floorPos.Count == 0) {
+                Debug.Log("couldn't find a good start pos");
+                continue;
+            }
+            if (numRooms > 1) {
+                var doorPos = RandomWalk.startReal + (RandomWalk.prevMove * -1);
+                doorGen(doorPos);
+            }
+            visited.UnionWith(floorPos);
+            floorsTotal.UnionWith(floorPos);
+            //generates floor
+            tileGen(floorTile, floorPos);
+            //generates roof
+            if(roof) {
+                roofGen(floorTile, floorPos);
+            }
+            //generates wall
+            wallGen(wallTile, floorPos);
+        }
+    }
+    private void doorGen(Vector3Int doorPos) {
+        //check to see if up,down,left,right contains either a wall tile or a door tile, if it does, generate a wall, if not, generate a door in the prefab
+        doorsTotal.Add(doorPos);
+        GameObject door = Instantiate(doorTile, doorPos + Vector3Int.up, Quaternion.identity);
+        tilesTotal.Add(door);
+    }
+
+    private void fillDoors() {
+        Debug.Log("fillDoors is called");
+        foreach (var door in doorsTotal) {
+            //create door up on Z
+            var doorCopy = door + Vector3Int.up;
+            if (floorsTotal.Contains(door + cardinalDirectionsList[0])) {
+                var doorBlock = Instantiate(doorZ, new Vector3(doorCopy.x, doorCopy.y - 0.2f, doorCopy.z + 0.475f), Quaternion.identity);
+                tilesTotal.Add(doorBlock);
+            }
+            else {
+                var wallBlock = Instantiate(wallZ, new Vector3(doorCopy.x, doorCopy.y, doorCopy.z + 0.475f), Quaternion.identity);
+                tilesTotal.Add(wallBlock);
+            }
+            //create door down on Z
+            if (floorsTotal.Contains(door + cardinalDirectionsList[1])) {
+                var doorBlock = Instantiate(doorZ, new Vector3(doorCopy.x, doorCopy.y - 0.2f, doorCopy.z - 0.475f), Quaternion.identity);
+                tilesTotal.Add(doorBlock);
+            }
+            else {
+                var wallBlock = Instantiate(wallZ, new Vector3(doorCopy.x, doorCopy.y, doorCopy.z - 0.475f), Quaternion.identity);
+                tilesTotal.Add(wallBlock);
+            }
+            //create door up on X
+            if (floorsTotal.Contains(door + cardinalDirectionsList[2])) {
+                var doorBlock = Instantiate(doorX, new Vector3(doorCopy.x + 0.475f, doorCopy.y - 0.2f, doorCopy.z), Quaternion.identity);
+                tilesTotal.Add(doorBlock);
+            }
+            else {
+                var wallBlock = Instantiate(wallX, new Vector3(doorCopy.x + 0.475f, doorCopy.y, doorCopy.z), Quaternion.identity);
+                tilesTotal.Add(wallBlock);
+            }
+            //create door down on X
+            if (floorsTotal.Contains(door + cardinalDirectionsList[3])) {
+                var doorBlock = Instantiate(doorX, new Vector3(doorCopy.x - 0.475f, doorCopy.y - 0.2f, doorCopy.z), Quaternion.identity);
+                tilesTotal.Add(doorBlock);
+            }
+            else {
+                var wallBlock = Instantiate(wallX, new Vector3(doorCopy.x - 0.475f, doorCopy.y, doorCopy.z), Quaternion.identity);
+                tilesTotal.Add(wallBlock);
+            }
+        }
+    }
+    private void starterGen(GameObject tile) {
+        HashSet<Vector3Int> floorPos = new HashSet<Vector3Int>();
+        for (int i = -2; i <= 2; i++) {
+            for (int j = -2; j <= 2; j++) {
+                floorPos.Add(new Vector3Int(i, 0, j));
+            }
+        }
+        doorGen(new Vector3Int(3, 0, 0));
+        doorGen(new Vector3Int(0, 0, 3));
+        doorGen(new Vector3Int(0, 0, -3));
+
+        roomGen(1, new Vector3Int(4, 0, 0));
+        roomGen(1, new Vector3Int(0, 0, 4));
+        roomGen(1, new Vector3Int(0, 0, -4));
+        visited.UnionWith(floorPos);
+        floorsTotal.UnionWith(floorPos);
+        tileGen(tile, floorPos);
+        roofGen(tile, floorPos);
+    }
+
+    private void wallGen(GameObject tile, HashSet<Vector3Int> floorPos) {
+        HashSet<Vector3Int> wallPos = new HashSet<Vector3Int>();
+        foreach (var floor in floorPos) {
+            var tileUp = floor + cardinalDirectionsList[0];
+            var tileDown = floor + cardinalDirectionsList[1];
+            var tileRight = floor + cardinalDirectionsList[2];
+            var tileLeft = floor + cardinalDirectionsList[3];
+
+            if(!floorPos.Contains(tileUp)) {
+                wallPos.Add(tileUp);
+            }
+            if (!floorPos.Contains(tileDown)) {
+                wallPos.Add(tileDown);
+            }
+            if (!floorPos.Contains(tileRight)) {
+                wallPos.Add(tileRight);
+            }
+            if (!floorPos.Contains(tileLeft)) {
+                wallPos.Add(tileLeft);
+            }
+        }
+        visited.UnionWith(wallPos);
+        wallsTotal.UnionWith(wallPos);
+    }
+
+}
---
+++ Assets/Scripts/RandomWalk.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class RandomWalk {
+    public static Vector3Int startReal;
+    public static Vector3Int prevMove;
+
+    public static List<Vector3Int> cardinalDirectionsList = new List<Vector3Int>()
+    {
+        new Vector3Int(0,0,1), //UP
+        new Vector3Int(0,0,-1), //DOWN
+        new Vector3Int(1,0,0), //RIGHT
+        new Vector3Int(-1,0,0) //LEFT
+    };
+    public static HashSet<Vector3Int> randomWalk(Vector3Int startPos, int walkLength, int maxLen, HashSet<Vector3Int> walkedSet) {
+        HashSet<Vector3Int> floorPos = new HashSet<Vector3Int>();
+
+        int attempts1 = 0;
+        //if start pos is in a taken space, move up, down, right, or left until you reach a free space.
+        while ((walkedSet.Contains(startPos) || Mathf.Abs(startPos.x) <= 3 && Mathf.Abs(startPos.z) <= 3) && attempts1 < 100) {
+            Debug.Log("Started in a taken space");
+            Vector3Int shift = cardinalDirectionsList[Random.Range(0, 3)];
+            startPos += shift;
+            prevMove = shift;
+            attempts1++;
+        }
+
+        if(attempts1 >= 100) {
+            return floorPos;
+        }
+        floorPos.Add(startPos);
+        startReal = startPos;
+
+        //Debug.Log("start pos: " + startPos);
+
+        Vector3Int prevPos = startPos;
+
+        for (int i = 0; i < walkLength; i++) {
+            int attempts = 0;
+            Vector3Int newPos;
+            do {
+                Vector3Int direction = cardinalDirectionsList[Random.Range(0, cardinalDirectionsList.Count)];
+                newPos = prevPos + direction;
+                attempts++;
+            } while ((walkedSet.Contains(newPos) || Mathf.Abs(newPos.x - startPos.x) > maxLen || Mathf.Abs(newPos.z - startPos.z) > maxLen || Mathf.Abs(newPos.x) <= 3 && Mathf.Abs(newPos.z) <= 3) && attempts < 100);
+
+            if (attempts < 100) {
+                floorPos.Add(newPos);
+                prevPos = newPos;
+                //Debug.Log("Added newPos at: " + newPos);
+            }
+            else {
+                // Dead-end, choose a random previous floor tile to resume from
+                prevPos = floorPos.ElementAt(Random.Range(0, floorPos.Count));
+                //Debug.Log("Too many attempts. Jumping to random floorPos.");
+            }
+        }
+
+        return floorPos;
+    }
+}
